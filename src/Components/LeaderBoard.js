bb8218b6edea20e698bc0d223b648ccc4cb6fce9
faiqa1 <faiqarao03@gmail.com>
2024-05-16 17:52:44 +0500

    testing init commit

--- src/Components/LeaderBoard.js
+++ src/Components/LeaderBoard.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function LeaderBoard() {
   return (
     <div>
-      
+      <h2>git commit</h2>
+      <h2>git commit</h2>
+      <h2>git commit</h2> 
     </div>
   )
 }
